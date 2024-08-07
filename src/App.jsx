a20b6381e73db1a492f098b8eba46656120ed024
soniji03 <jbpomsoni03@gmail.com>
2024-08-07 18:55:02 +0530

    save music

--- src/App.jsx
+++ src/App.jsx
@@ -7,43 +7,43 @@ function App() {
   const tracks = [
     { 
       title: "Hanuman Chalisa", 
-      src: "/Hanuman_Chalisa_Hariharan,_Gulshan_Kumar_192_MyMp3Bhojpuri_In.mp3" 
+      src: encodeURIComponent("/Hanuman_Chalisa_Hariharan,_Gulshan_Kumar_192_MyMp3Bhojpuri_In.mp3" )
     },
     { 
       title: "Zindagi Ka Safar Hai Yeh Kaisa Safar", 
-      src: "/01. Zindagi Ka Safar Hai Yeh Kaisa Safar.mp3" 
+      src: encodeURIComponent("/01. Zindagi Ka Safar Hai Yeh Kaisa Safar.mp3" )
     }, { 
       title: "Tum Aa Gaye Ho Noor Aa Gaya", 
-      src: "/05. Tum Aa Gaye Ho Noor Aa Gaya.mp3" 
+      src: encodeURIComponent("/05. Tum Aa Gaye Ho Noor Aa Gaya.mp3" )
     }, { 
       title: "Chura Liya Hai Tumne ", 
-      src: "/Chura Liya Hai Tumne (Yaadon Ki Baraat) (PagalWorld.com).mp3" 
+      src: encodeURIComponent ("/Chura Liya Hai Tumne (Yaadon Ki Baraat) (PagalWorld.com).mp3" )
     }, { 
       title: "Ghar Se Nikalte Hi", 
-      src: "/Ghar Se Nikalte Hi - Armaan Malik 320Kbps.mp3" 
+      src: encodeURIComponent("/Ghar Se Nikalte Hi - Armaan Malik 320Kbps.mp3" )
     }, { 
       title: "Hamare sath shree raghunath", 
-      src: "/Hamare-sath-shree-raghunath.mp3" 
+      src: encodeURIComponent("/Hamare-sath-shree-raghunath.mp3" )
     }, { 
       title: "Kisi Shayar Ka Dil Banke", 
-      src: "/Kisi Shayar Ka Dil Banke_320(PaglaSongs).mp3" 
+      src: encodeURIComponent("/Kisi Shayar Ka Dil Banke_320(PaglaSongs).mp3" )
     },
     { 
       title: "Sankatmochan Hanuman Ashtak ", 
-      src: "/new_128_Sankatmochan Hanuman Ashtak - Hariharan.mp3" 
+      src: encodeURIComponent("/new_128_Sankatmochan Hanuman Ashtak - Hariharan.mp3" )
     }, { 
       title: "O Mere Sona Re", 
-      src: "/O Mere Sona Re - King-(DJMaza).mp3" 
+      src: encodeURIComponent("/O Mere Sona Re - King-(DJMaza).mp3") 
     }, { 
       title: "O Mere Sona Re Sona Teesri Manzil", 
-      src: "/O Mere Sona Re Sona Teesri Manzil 128 Kbps.mp3" 
+      src: encodeURIComponent("/O Mere Sona Re Sona Teesri Manzil 128 Kbps.mp3" )
     }, { 
       title: "Sajdaa", 
-      src: "/Sajdaa - Rahat Fateh Ali Khan 320Kbps.mp3" 
+      src: encodeURIComponent("/Sajdaa - Rahat Fateh Ali Khan 320Kbps.mp3" )
     },
     { 
       title: "Bajrang Baan", 
-      src: "/Om Cham Cham Chapal Chalanta – Bajrang Baan.mp3" 
+      src: encodeURIComponent("/Om Cham Cham Chapal Chalanta – Bajrang Baan.mp3" )
     },
     
   ];
